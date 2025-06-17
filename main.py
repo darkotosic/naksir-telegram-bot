bab5003d3fe2c10b771f4c129a4fec0ed3bfb13e
darkotosic <darkotosic1986@gmail.com>
2025-06-17 14:34:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-from commands.start import start
+from commands.start import start_command
 from commands.help import help_command
 from commands.analyze import analyze
 from commands.analyze_auto import analyze_auto
@@ -16,7 +16,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("analyze", analyze))            # /analyze <fixture_id>
 app.add_handler(CommandHandler("analyze_auto", analyze_auto))  # /analyze_auto
