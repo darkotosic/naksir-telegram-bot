add3f22ac6b43c5919d963a8d5048854cf3ba3ea
darkotosic <darkotosic1986@gmail.com>
2025-06-17 20:39:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, CommandHandler
-
-from commands.start import start_command as start
+from telegram.ext import Application, CommandHandler
+from commands.start import start_command
 from commands.help import help_command
-from commands.analyze import analyze_command as analyze
-from commands.analyze_auto import analyze_auto
-
+from commands.analyze import analyze_command
+from commands.analyze_auto import analyze_auto_command
 from utils.logger import get_logger
 
 load_dotenv()
 logger = get_logger()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("analyze", analyze))
-app.add_handler(CommandHandler("analyze_auto", analyze_auto))
 
 if __name__ == "__main__":
-    logger.info("🚀 Bot is starting...")
-    print("🚀 Bot is running...")
+    logger.info("🚀 Starting bot...")
+
+    app = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("analyze", analyze_command))            # /analyze <fixture_id>
+    app.add_handler(CommandHandler("analyze_auto", analyze_auto_command))  # /analyze_auto
+
     app.run_polling()
