fef8760079d8d3ab9139b058f0e419b448300509
darkotosic <darkotosic1986@gmail.com>
2025-06-17 12:47:44 +0200

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,17 @@
+# utils/logger.py
+
+import logging
+
+def get_logger(name: str = "naksir-bot"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
