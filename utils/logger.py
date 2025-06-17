926a5b801b1ed4824ebd0349f799af81cc6fa100
darkotosic <darkotosic1986@gmail.com>
2025-06-17 12:51:18 +0200

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,17 +1,26 @@
-# utils/logger.py
-
 import logging
+import os
 
-def get_logger(name: str = "naksir-bot"):
+def get_logger(name: str = "naksir"):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
+    # Izbegni duplo dodavanje handlera
     if not logger.handlers:
-        handler = logging.StreamHandler()
+        # Formatter
         formatter = logging.Formatter(
-            "[%(asctime)s] [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+            "[%(asctime)s] [%(levelname)s] - %(message)s", "%Y-%m-%d %H:%M:%S"
         )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+
+        # Stream (terminal) handler
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        # File handler
+        os.makedirs("logs", exist_ok=True)
+        file_handler = logging.FileHandler("logs/naksir.log", mode='a', encoding='utf-8')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
     return logger
