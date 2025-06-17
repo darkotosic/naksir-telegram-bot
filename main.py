62773b6a85454d5208a83293be37dded10c175f1
darkotosic <darkotosic1986@gmail.com>
2025-06-17 22:22:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 if __name__ == "__main__":
     logger.info("🚀 Starting bot...")
 
-    app = Application.builder().token(TELEGRAM_TOKEN).build()
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
-    app.add_handler(CommandHandler("analyze", analyze_command))            # /analyze <fixture_id>
-    app.add_handler(CommandHandler("analyze_auto", analyze_auto_command))  # /analyze_auto
+    app.add_handler(CommandHandler("analyze", analyze))            # /analyze <fixture_id>
+    app.add_handler(CommandHandler("analyze_auto", analyze_auto))  # /analyze_auto
 
     app.run_polling()
